2a4af21c0d4502c177fdcbf78fa249febeb761d0
Daniel-maker359 <Danielbilge19@hotmail.com>
2025-01-07 14:27:33 +0100

    hej kommentar

--- NGO2024/src/ngo2024/NGO2024.java
+++ NGO2024/src/ngo2024/NGO2024.java
@@ -14,6 +14,7 @@ public class NGO2024 {
      * @param args the command line arguments
      */
     //test
+    //hej
     public static void main(String[] args) {
         // TODO code application logic here
     }
