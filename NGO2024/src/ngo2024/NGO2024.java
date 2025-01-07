246c018655deadb8e57a2a86b7759acddd78fdea
Seansiik <Seansiik@hotmail.com>
2025-01-07 14:26:11 +0100

    test

--- NGO2024/src/ngo2024/NGO2024.java
+++ NGO2024/src/ngo2024/NGO2024.java
@@ -13,6 +13,7 @@ public class NGO2024 {
     /**
      * @param args the command line arguments
      */
+    //test
     public static void main(String[] args) {
         // TODO code application logic here
     }
