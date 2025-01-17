9a0a4474accc613c83b1987c1f5ff423ae7bbc16
Tysonx10 <afsharityson@gmail.com>
2025-01-17 03:22:19 +0100

    tgfg

--- NGO2024/src/ngo2024/MinaProjekt.java
+++ NGO2024/src/ngo2024/MinaProjekt.java
@@ -96,7 +96,7 @@ public class MinaProjekt extends javax.swing.JFrame {
                 {null, null, null, null}
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "Namn", "Title 2", "Title 3", "Title 4"
             }
         ));
         jScrollPane1.setViewportView(jTable1);
